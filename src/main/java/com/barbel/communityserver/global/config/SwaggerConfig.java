e311f9c23f6b30f78ed654bbc9f349f879c7f7bf
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-06-03 02:21:15 +0900

    [FEAT/API] set

--- src/main/java/com/barbel/communityserver/global/config/SwaggerConfig.java
+++ src/main/java/com/barbel/communityserver/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.barbel.communityserver;
+package com.barbel.communityserver.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
